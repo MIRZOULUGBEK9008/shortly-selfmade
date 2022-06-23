2d1f99e30449bc6f18b503105b3f47f1edb581f9
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-06-23 18:53:07 +0500

    Add js part into url-shortener.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
+const elUrlSHortenerResults = document.querySelector('.url-shortener-form__results-wrapper');
+const elUrlShortenerCopyButton = document.querySelector('.js-button-copy-clipboard');
+
+elUrlShortenerForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  elUrlSHortenerResults.classList.add('url-shortener-form__results-wrapper--open');
+});
+
+elUrlShortenerForm.addEventListener('click', function (evt) {
+  if (evt.target.matches('.js-button-copy-clipboard')) {
+    evt.target.textContent = 'Copied!';
+
+    evt.target.classList.add('button--rectangle-small-copied');
+
+    setTimeout(function () {
+      evt.target.textContent = 'Copy';
+      evt.target.classList.remove('button--rectangle-small-copied');
+    }, 1000);
+
+  }
+});
